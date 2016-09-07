3c2b036be6d61f469ad446a4b7c9b041d21bbcf1
Spencer Watson <spencer@wattydev.com>
2016-09-06 20:21:43 -0700

    define task model

--- exercise-app/app/models/task.js
+++ exercise-app/app/models/task.js
@@ -1,6 +1,54 @@
 import Ember from 'ember';
 
 export default Ember.Object.extend({
+    /**
+     * The title of the task.
+     *
+     * @property title
+     * @type {String}
+     */
     title: null,
 
+    /**
+     * The days in which the task should be completed.
+     * 0 = Sunday, 1 = Monday, etc...
+     *
+     * @property days
+     * @type {Number[]}
+     */
+    days: Ember.A(),
+
+    /**
+     * The order in which the task should be completed.
+     * Task will appear once for each ordering number.
+     *
+     * @property order
+     * @type {Number[]}
+     */
+    order: Ember.A(),
+
+    /**
+     * The amount of time to provide for completing the task (ms).
+     *
+     * @property timer
+     * @type {Number}
+     */
+    timer: null,
+
+    /**
+     * The number of repetitions that the user should make to complete the exercise.
+     *
+     * @property reps
+     * @type {Number}
+     */
+    reps: null,
+
+    /**
+     * The amount of weight to apply to the exercise (lbs).
+     * This value may get overwritten by one saved in local storage.
+     *
+     * @property weight
+     * @type {Number}
+     */
+    weight: null
 });
